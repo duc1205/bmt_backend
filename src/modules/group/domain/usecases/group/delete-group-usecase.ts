456ce0d795889683dc1aa963dde544e35f7f6ba1
duy <duybb@suga.com.vn>
2023-10-04 10:41:45 +0700

    Update delete group

--- src/modules/group/domain/usecases/group/delete-group-usecase.ts
+++ src/modules/group/domain/usecases/group/delete-group-usecase.ts
@@ -8,6 +8,12 @@ import { LogicalException } from 'src/exceptions/logical-exception';
 import { DeleteAllGroupMemberUsecase } from '../group-member/delete-all-group-member-usecase';
 import { Transactional } from 'typeorm-transactional';
 import { UserModel } from 'src/modules/user/domain/models/user-model';
+import { GetEventsUsecase } from 'src/modules/event/domain/usecases/event/get-events-usecase';
+import { PaginationParams } from 'src/core/models/pagination-params';
+import { SortParams } from 'src/core/models/sort-params';
+import { EventStatus } from 'src/modules/event/enum/event-status-enum';
+import { EventScope } from 'src/modules/event/enum/event-scope-enum';
+import { throwError } from 'src/core/helpers/utils';
 
 @Injectable()
 export class DeleteGroupUsecase {
@@ -16,12 +22,30 @@ export class DeleteGroupUsecase {
     private readonly groupRepository: GroupRepository,
     private readonly deleteEventMembersInGroupUsecase: DeleteEventMembersInGroupUsecase,
     private readonly deleteAllEventsInGroupUsecase: DeleteAllEventsInGroupUsecase,
+    private readonly getEventsUsecase: GetEventsUsecase,
   ) {}
 
   @Transactional()
   async call(group: GroupModel, owner: UserModel | undefined): Promise<void> {
     if (owner && group.ownerId != owner.id) {
       throw new LogicalException(ErrorCode.GROUP_NOT_BELONG_TO_YOUR_OWN, 'Group not belong to your own.', undefined);
+    }
+
+    const events = await this.getEventsUsecase.call(
+      new PaginationParams(1, 1, true, undefined),
+      new SortParams(undefined, undefined),
+      undefined,
+      group,
+      EventStatus.isHappening,
+      EventScope.Group,
+      undefined,
+    );
+    if (events.totalCount ?? throwError() > 0) {
+      throw new LogicalException(
+        ErrorCode.GROUP_CAN_NOT_DELETE,
+        'Can not delete because event group is happening.',
+        undefined,
+      );
     }
 
     await this.deleteEventMembersInGroupUsecase.call(group, undefined, undefined);
